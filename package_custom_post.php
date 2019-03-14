52f65978862c2cf1e1d96cafc549acb5b34a3c15
technote <technote.space@gmail.com>
2019-03-15 03:19:19 +0900

    v0.0.26

--- package_custom_post.php
+++ package_custom_post.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework Package Custom_Post
  *
- * @version 0.0.21
+ * @version 0.0.26
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
