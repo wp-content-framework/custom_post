7ee88724059302ed0c70c4b6b9ba88574a1696f1
technote <technote.space@gmail.com>
2019-03-18 01:55:09 +0900

    v0.0.29

--- src/classes/models/custom_post.php
+++ src/classes/models/custom_post.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Custom_Post Classes Models Custom Post
  *
- * @version 0.0.28
+ * @version 0.0.29
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
