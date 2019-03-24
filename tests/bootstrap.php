e6e1f220964ed02f0192cb592477fe08b91a7ea5
Technote <technote.space@gmail.com>
2019-03-24 15:15:15 +0900

    v0.0.31

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework Test Bootstrap
  *
- * @version 0.0.26
+ * @version 0.0.31
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
