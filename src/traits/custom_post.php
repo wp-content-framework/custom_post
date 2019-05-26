1a0b30e1acc84f781488707ca764a7f3a7493649
Technote <technote.space@gmail.com>
2019-05-26 21:38:11 +0900

    v0.0.36

--- src/traits/custom_post.php
+++ src/traits/custom_post.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Custom_Post Traits Custom Post
  *
- * @version 0.0.35
+ * @version 0.0.36
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
