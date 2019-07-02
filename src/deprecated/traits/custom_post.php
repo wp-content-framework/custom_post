4fc412832dfcde9fe383d458447b00a649bf94ac
Technote <technote.space@gmail.com>
2019-07-03 01:30:25 +0900

    feat: remove versino info (wp-content-framework/core#205)

--- src/deprecated/traits/custom_post.php
+++ src/deprecated/traits/custom_post.php
@@ -2,7 +2,6 @@
 /**
  * WP_Framework_Custom_Post Deprecated Traits Custom Post
  *
- * @version 0.0.34
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
